7bccb3f181fb091d5012cd5541930928d8425480
R B SRIRAM <sriramrb88@hotmail.com>
2025-08-08 21:07:08 +0530

    Data type list and dict

---
+++ more_data_types.py
@@ -0,0 +1,28 @@
+# arr = []
+# arr.append(123)
+# arr.append(234)
+# arr.append("sriram")
+#
+# print(arr)
+# print(f"type of arr[1] is {type(arr[1])}")
+# print(f"type of arr[1] is {type(arr[2])}")
+
+# marks = {}
+# print (type(marks))
+#
+# marks["Mathamatics"] = 99
+# marks["Science"] = 92
+# marks["Social"] = 82.5
+# marks[123] =100
+# print (marks)
+
+# list_to_sum = {}
+#
+# arr = [1,2,3,4,5]
+# list_to_sum[arr] = 6
+#
+# print (list_to_sum)
+
+int_to_arr = {}
+
+int
